b9ba77ae12ee7830e503c3aadc6f4fefeee788a5
Ridge Kimani <ridgekimani@gmail.com>
2023-10-15 21:48:27 +0300

    feat: add author modal

--- pages/index.vue
+++ pages/index.vue
@@ -11,20 +11,39 @@
       <div>
         <b-button variant="success" v-b-modal.add-author>Add Author</b-button>
 
-        <b-modal id="add-author" title="Add Author">
+        <b-modal id="add-author" title="Add Author" @cancel='cancel'>
           <div>
             <b-form>
-              <b-form-group id="input-group-2" label="Name:" label-for="input-2">
-                <b-form-input
-                  id="input-2"
-                  placeholder="Enter name"
-                  required
-                ></b-form-input>
+              <b-form-group id="name-group" label="Name:" label-for="name">
+                <b-form-input id="name" placeholder="Enter name" required v-model="name"></b-form-input>
               </b-form-group>
-
+              <div class="my-4">
+                <b-button variant="success" v-b-modal.add-book>Add Book</b-button>
+              </div>
               <b-table striped hover :items="items" :fields="fields"></b-table>
             </b-form>
+          </div>
+        </b-modal>
 
+        <b-modal id="add-book" title="Add Book" @ok="addBook">
+          <div>
+            <b-form>
+              <b-form-group id="title-group" label="Title:" label-for="title">
+                <b-form-input id="title" placeholder="Enter book title" required v-model="book.title"></b-form-input>
+              </b-form-group>
+              <b-form-group id="isbn-group" label="Isbn:" label-for="isbn">
+                <b-form-input id="isbn" v-model="book.isbn" placeholder="ISBN"></b-form-input>
+              </b-form-group>
+              <b-form-group id="pages-group" label="Pages:" label-for="pages">
+                <b-form-input id="pages" placeholder="Pages" v-model="book.pages"></b-form-input>
+              </b-form-group>
+              <b-form-group id="group-year" label="Publish Year:" label-for="year">
+                <b-form-input id="year" placeholder="Enter name" v-model="book.year"></b-form-input>
+              </b-form-group>
+              <b-form-group id="cost-group" label="Cost:" label-for="cost">
+                <b-form-input id="cost" placeholder="Cost" v-model="book.cost"></b-form-input>
+              </b-form-group>
+            </b-form>
           </div>
         </b-modal>
       </div>
@@ -46,7 +65,54 @@ export default {
 
   data: () => ({
     items: [{}],
-    fields: ['name', 'author', 'isbn', 'pages', 'cost']
-  })
+    fields: ['title', 'author', 'isbn', 'pages', 'cost'],
+    name: '',
+    book: {
+      title: '',
+      isbn: '',
+      cost: '',
+      year: '',
+      pages: ''
+    }
+  }),
+
+  methods: {
+    addBook() {
+      this.items.push({ ...this.book, author: this.name });
+      this.resetBook();
+    },
+
+    resetBook() {
+      this.book = {
+        title: '',
+        isbn: '',
+        cost: '',
+        year: '',
+        pages: ''
+      }
+    },
+
+    resetItems() {
+      this.items = [{}]
+    },
+
+    resetAuthor() {
+      this.name = ''
+    },
+
+    cancel() {
+      this.resetItems()
+      this.resetBook()
+      this.resetAuthor()
+    }
+  },
+
+  watch: {
+    name: {
+      handler(author) {
+        this.items = this.items.map((item) => ({ ...item, author: item.title ? author : null }));
+      }
+    }
+  }
 };
 </script>
